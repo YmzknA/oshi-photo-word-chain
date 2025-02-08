e04946f405f351888bbeff2c6ccc0e4d934ba1ea
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-02-08 18:08:06 +0900

    devise:controller,vier作成、そのほか、toppage等デザイン

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
 
   # nameの長さは1文字以上50文字以下
   # bodyの長さは1文字以上50文字以下
-  validates :name, length: { in: 1..50 }
-  validates :body, length: { in: 0..50 }
+  validates :name, length: { in: 1..20 }
+  validates :body, length: { in: 0..20 }
 
   mount_uploader :image, ImageUploader
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "homes/top"
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,5 +12,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "home#top"
+  root "homes#top"
 end
