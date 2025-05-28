862d3ce487d952ff1472fa836f151d29aa7a79b2
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-05-28 18:26:17 +0900

    fix: url保存時にバリデーション、およびポスト失敗時にフォームが初期化される問題を修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   validates :name, length: { in: 2..20 }
   validates :body, length: { in: 0..200 }
   validates :url, length: { in: 0..2083 }
+  validate :url_format_validation, if: -> { url.present? }
 
   mount_uploader :image, ImageUploader
 
@@ -13,5 +14,16 @@ class Post < ApplicationRecord
 
   def self.all_posts
     Post.all.order(created_at: :desc)
+  end
+
+  private
+
+  def url_format_validation
+    return if url.blank?
+
+    uri = URI.parse(url)
+    errors.add(:url, 'is not a valid URL') unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  rescue URI::InvalidURIError
+    errors.add(:url, 'is not in the correct format')
   end
 end
